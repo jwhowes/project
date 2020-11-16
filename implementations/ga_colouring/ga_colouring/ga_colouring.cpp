38dd87c1c94cf7a8f42bab5629c6b9fad41e6512
jwhowes <jacob.w.howes@durham.ac.uk>
2020-11-16 18:41:54 +0000

    Implemented mutation and sorting just need to do parent selection

--- implementations/ga_colouring/ga_colouring/ga_colouring.cpp
+++ implementations/ga_colouring/ga_colouring/ga_colouring.cpp
@@ -16,6 +16,10 @@ using namespace boost::random;
 const int k = 3;
 const int NUM_VERTICES = 10;
 
+struct Partition {
+	vector<int> partition[k];
+};
+
 int adj_matrix[NUM_VERTICES][NUM_VERTICES] = {
 	{0, 1, 0, 0, 1, 1, 0, 0, 0, 0},
 	{1, 0, 1, 0, 0, 0, 1, 0, 0, 0},
@@ -31,9 +35,10 @@ int adj_matrix[NUM_VERTICES][NUM_VERTICES] = {
 
 const int pop_size = 50;
 const int num_iterations = 100;
-const float mutation_prob = 0.1;
+const float mutation_prob = 0.1f;
 
-vector<int> population[NUM_VERTICES][k];
+Partition population[pop_size];
+Partition new_population[pop_size];
 
 mt19937 seed;
 uniform_real_distribution<float> uni(0, 1);
@@ -100,33 +105,44 @@ void generate_member(vector<int> * x) {
 	}
 }
 
+void mutate(vector<int> * x){  // Randomly moves a vertex from one colour class to another
+	int c = random_colour(seed);
+	while (x[c].size() == 0) {
+		c = (c + 1) % k;
+	}
+	int d = random_colour(seed);
+	int v = x[c][uniform_int_distribution<int>(0, x[c].size() - 1)(seed)];
+	x[c].erase(remove(x[c].begin(), x[c].end(), v), x[c].end());
+	x[d].push_back(v);
+}
+
+bool compare_partitions(Partition & x, Partition & y) {
+	return f(x.partition) < f(y.partition);
+}
+
 int main(){
-	vector<int> p1[k];
-	vector<int> p2[k];
-	vector<int> x[k];
-	generate_member(p1);
-	generate_member(p2);
-	for (int c = 0; c < k; c++) {
-		for (int v = 0; v < p1[c].size(); v++) {
-			cout << p1[c][v] << " ";
+	int p1; int p2;
+	for (int i = 0; i < pop_size; i++) {
+		generate_member(population[i].partition);
+	}
+	for (int t = 0; t < num_iterations; t++) {
+		sort(begin(population), end(population), compare_partitions);  // You need to pass values through by reference (&), see if there's some way of doing this without defining a Partition struct
+		for (int i = 0; i < pop_size; i++) {
+			p1 = 0; p2 = 1;
+			gpx(population[p1].partition, population[p2].partition, new_population[i].partition);
+			if (uni(seed) < mutation_prob) {
+				mutate(new_population[i].partition);
+			}
+		}
+		for (int i = 0; i < pop_size; i++) {
+			copy(begin(new_population[i].partition), end(new_population[i].partition), begin(population[i].partition));
+		}
+	}
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < population[0].partition[i].size(); j++) {
+			cout << population[0].partition[i][j] << " ";
 		}
 		cout << endl;
 	}
-	cout << endl;
-	for (int c = 0; c < k; c++) {
-		for (int v = 0; v < p2[c].size(); v++) {
-			cout << p2[c][v] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	gpx(p1, p2, x);
-	for (int c = 0; c < k; c++) {
-		for (int v = 0; v < x[c].size(); v++) {
-			cout << x[c][v] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
 	return 0;
 }
