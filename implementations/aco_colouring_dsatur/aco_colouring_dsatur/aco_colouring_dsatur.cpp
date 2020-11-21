fca2ec9f9889ea4df585449c51d51bc4f60f8b3d
jwhowes <jacob.w.howes@durham.ac.uk>
2020-11-21 17:30:19 +0000

    Implemented vertex selection

--- implementations/aco_colouring_dsatur/aco_colouring_dsatur/aco_colouring_dsatur.cpp
+++ implementations/aco_colouring_dsatur/aco_colouring_dsatur/aco_colouring_dsatur.cpp
@@ -1,3 +1,5 @@
+#define _SECURE_SCL 0
+
 #include <iostream>
 #include <array>
 #include <vector>
@@ -7,6 +9,11 @@
 #include <boost/random/uniform_real_distribution.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
+
+// TODO:
+	// Implement eta
+	// Implement Daemon actions
+	// Implement pheromone evaporation
 
 using namespace std;
 using namespace boost::random;
@@ -32,9 +39,13 @@ const int num_ants = 50;
 const int num_iterations = 100;
 
 int ant_solution[num_ants][num_vertices];
+int ant_path[num_ants][num_vertices];  // Stores the path each ant takes through the construction graph (for Daemon actions)
 int ant_pos[num_ants];
 
-int best[num_vertices];
+const float rho = 0.5;
+const float q = 0.5;
+const float alpha = 1;
+const float beta = 1;
 
 mt19937 seed;
 uniform_real_distribution<float> uni(0, 1);
@@ -65,8 +76,15 @@ bool valid(int v, int c, int * col) {  // Returns whether or not vertex v can be
 	return true;  // If no such i can be found then the assignment is valid
 }
 
+float eta(int ant, int v) {
+	return 1;
+}
+
 int main(){
+	int best[num_vertices];
+	float weight[num_vertices];
 	initialise_pheromones();
+	int v;
 	for (int t = 0; t < num_iterations; t++) {
 		// Initialise ants (to a random vertex)
 		for (int i = 0; i < num_ants; i++) {
@@ -84,8 +102,39 @@ int main(){
 		for (int i = 0; i < num_vertices; i++) {
 			for (int a = 0; a < num_ants; a++) {
 				// Select next vertex with transition rule
+				float weight_sum = 0;
+				for (int u = 0; u < num_vertices; u++) {
+					weight[u] = pow(tau[ant_pos[a]][u], alpha) * pow(eta(a, u), beta);
+					weight_sum += weight[u];
+				}
+				if (uni(seed) < q) {
+					v = 0;
+					for (int u = 1; u < num_vertices; u++) {
+						if (weight[u] > weight[v]) {
+							v = u;
+						}
+					}
+				} else {
+					float r = uni(seed);
+					v = -1;
+					do {
+						v++;
+						r -= weight[v] / weight_sum;
+					} while (r > 0);
+				}
 				// Colour vertex lowest colour possible
+				int c = 0;
+				while (true) {
+					if (valid(v, c, ant_solution[a])) {
+						ant_solution[a][v] = c;
+						break;
+					}
+					c++;
+				}
 				// Apply online stage by stage pheromone update
+				tau[ant_pos[a]][v] = (1 - rho) * tau[ant_pos[a]][v] + rho;
+				ant_path[a][i] = ant_pos[a];
+				ant_pos[a] = v;
 			}
 		}
 		// Select best solution (update best if it's better)
