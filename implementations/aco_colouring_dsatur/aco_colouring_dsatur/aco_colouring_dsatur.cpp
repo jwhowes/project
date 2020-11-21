0124bf7662de6b86b0469159d53c837835d1b5b9
jwhowes <jacob.w.howes@durham.ac.uk>
2020-11-21 18:50:52 +0000

    Finished implementing, just have some bugs to work out

--- implementations/aco_colouring_dsatur/aco_colouring_dsatur/aco_colouring_dsatur.cpp
+++ implementations/aco_colouring_dsatur/aco_colouring_dsatur/aco_colouring_dsatur.cpp
@@ -10,10 +10,11 @@
 #include <boost/random/uniform_int_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
 
+// They use small ant populations (< 50) on very small graphs (|V| <= 100) so I don't think this is a particularly efficient algorithm
+
 // TODO:
-	// Implement eta
-	// Implement Daemon actions
-	// Implement pheromone evaporation
+	// Figure out why it's returning invalid colourings
+		// weight_sum is sometimes 0 (if I use tau + 1 in the transition rule then this doesn't happen)
 
 using namespace std;
 using namespace boost::random;
@@ -33,10 +34,10 @@ int adj_matrix[num_vertices][num_vertices] = {
 	{0, 0, 0, 0, 1, 0, 1, 1, 0, 0}
 };
 
-int tau[num_vertices][num_vertices];
+float tau[num_vertices][num_vertices];
 
 const int num_ants = 50;
-const int num_iterations = 100;
+const int num_iterations = 3000;
 
 int ant_solution[num_ants][num_vertices];
 int ant_path[num_ants][num_vertices];  // Stores the path each ant takes through the construction graph (for Daemon actions)
@@ -50,6 +51,16 @@ const float beta = 1;
 mt19937 seed;
 uniform_real_distribution<float> uni(0, 1);
 uniform_int_distribution<int> random_vertex(0, num_vertices - 1);
+
+int f(int * x) {
+	int max = 0;
+	for (int i = 0; i < num_vertices; i++) {
+		if (x[i] > max) {
+			max = x[i];
+		}
+	}
+	return max + 1;
+}
 
 void initialise_pheromones() {
 	for (int i = 0; i < num_vertices; i++) {
@@ -77,16 +88,25 @@ bool valid(int v, int c, int * col) {  // Returns whether or not vertex v can be
 }
 
 float eta(int ant, int v) {
-	return 1;
+	// Returns the heuristic value of v in colouring ant
+	vector<int> neighbouring_colours;
+	for (int i = 0; i < num_vertices; i++) {
+		if (adj_matrix[v][i] == 1 && ant_solution[ant][i] != -1 && find(neighbouring_colours.begin(), neighbouring_colours.end(), ant_solution[ant][i]) == neighbouring_colours.end()) {
+			neighbouring_colours.push_back(ant_solution[ant][i]);
+		}
+	}
+	return neighbouring_colours.size();
 }
 
 int main(){
 	int best[num_vertices];
+	int f_best;
 	float weight[num_vertices];
 	initialise_pheromones();
 	int v;
 	for (int t = 0; t < num_iterations; t++) {
 		// Initialise ants (to a random vertex)
+		initialise_ants();
 		for (int i = 0; i < num_ants; i++) {
 			ant_pos[i] = random_vertex(seed);
 			int c = 0;
@@ -104,8 +124,12 @@ int main(){
 				// Select next vertex with transition rule
 				float weight_sum = 0;
 				for (int u = 0; u < num_vertices; u++) {
-					weight[u] = pow(tau[ant_pos[a]][u], alpha) * pow(eta(a, u), beta);
-					weight_sum += weight[u];
+					if (ant_solution[a][u] != -1) {
+						weight[u] = 0;
+					} else {
+						weight[u] = pow(tau[ant_pos[a]][u], alpha) * pow(eta(a, u) + 1, beta);
+						weight_sum += weight[u];
+					}
 				}
 				if (uni(seed) < q) {
 					v = 0;
@@ -137,10 +161,35 @@ int main(){
 				ant_pos[a] = v;
 			}
 		}
-		// Select best solution (update best if it's better)
+		// Find best solution (and update best if it's better
+		int b = 0;
+		int f_b = f(ant_solution[0]);
+		for(int i = 1; i < num_ants; i++){
+			int f_i = f(ant_solution[i]);
+			if (f_i < f_b) {
+				b = i;
+				f_b = f_i;
+			}
+		}
+		if (t == 0 || f_b < f_best) {
+			copy(begin(ant_solution[b]), end(ant_solution[b]), begin(best));
+			f_best = f_b;
+		}
 		// Apply Daemon actions
+		for (int i = 0; i < num_vertices - 1; i++) {
+			tau[ant_path[b][i]][ant_path[b][i + 1]] += rho * f_b;
+		}
 		// Apply pheromone evaporation
+		for (int i = 0; i < num_vertices; i++) {
+			for (int j = 0; j < num_vertices; j++) {
+				tau[i][j] *= 1 - rho;
+			}
+		}
 	}
 	// Return best solution
+	for (int i = 0; i < num_vertices; i++) {
+		cout << best[i] << " ";
+	}
+	cout << endl << "Number of colours: " << f_best;
 	return 0;
 }
